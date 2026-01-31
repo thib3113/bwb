f72b2c3bfd87d814877098aa4fde0869b29a1d0f
thib3113 <thib3113@users.noreply.github.com>
2026-01-31 19:31:38 +0000

    deploy: 0841dec04c19e183b33ef4e748e56821be3a6042

---
+++ pr-1/assets/SettingsContentWrapper-C8halcV1.js
@@ -0,0 +1,1 @@
+import{b as t,bh as n,o as e}from"./index-BIgZae4C.js";const r=()=>t(e,{sx:{p:2},children:t(n,{})});export{r as SettingsContentWrapper};
--- pr-1/assets/SettingsContentWrapper-CcciWE37.js
+++
@@ -1,1 +0,0 @@
-import{b as t,bh as n,o as e}from"./index-ChRZ5Hco.js";const r=()=>t(e,{sx:{p:2},children:t(n,{})});export{r as SettingsContentWrapper};
